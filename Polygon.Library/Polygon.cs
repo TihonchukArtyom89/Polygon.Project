7368ed1b9958cb57f90c2b6d12fe33aea94b714f
TihonchukArtyom89 <mortus1989@gmail.com>
2024-02-04 16:01:54 +0300

    Добавьте файлы проекта.

---
+++ Polygon.Library/Polygon.cs
@@ -0,0 +1,38 @@
+﻿namespace Polygon.Library
+{
+    public class Point//класс для опредения точки координат вершины многоугольника 
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int? x=0,int? y=0)
+        {
+            X = x ?? 0;
+            Y = y ?? 0;
+        }
+    }
+    public abstract class Polygon //класс определения понятия многоугольник, вычисление площади многоугольник по формуле шнуровка гаусса (родительский класс для более конкретных фигур), определение выпуклости многоугольника, поле для длины массива
+    {
+        public int Length { get; set; }
+        public Point[]? points { get; set; }
+        public Polygon(Point[]? points)
+        {
+            if( points is not null )
+            {
+                Length = points.Length;
+                points = new Point[points.Length];
+            }
+            else
+            {
+                throw new NullReferenceException("Не задан массив координат вершин многоугольника.");
+            }
+        }
+    }
+    public class Circle : Polygon//класс для вычисления площади круга (2*pi*r),массив вершин имеет длину в 1 элемент, поле для константы пи,поле для радиуса ,перегрузка метода вычисления площади 
+    {
+
+    }
+    public class Triangle : Polygon//класс для вычисления площади (ф-ла из класса Polygon),массив вершин имеет длину в 3 элемента, проверка треугольника на прямоугольность
+    {
+
+    }
+}
