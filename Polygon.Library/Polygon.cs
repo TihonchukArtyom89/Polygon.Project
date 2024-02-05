c17290a9f6f953c8266cc558a35e2eff371c1e3b
TihonchukArtyom89 <mortus1989@gmail.com>
2024-02-05 16:54:54 +0300

    поработал над классом окружности(исправить ошибки в конструкторе)

--- Polygon.Library/Polygon.cs
+++ Polygon.Library/Polygon.cs
@@ -1,76 +1,92 @@
-﻿namespace Polygon.Library
+﻿namespace Polygon.Library;
+
+public class Point//класс для опредения точки координат вершины многоугольника 
 {
-    public class Point//класс для опредения точки координат вершины многоугольника 
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Point(int? x = 0, int? y = 0)
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public Point(int? x = 0, int? y = 0)
+        X = x ?? 0;
+        Y = y ?? 0;
+    }
+}
+public abstract class Polygon //класс определения понятия многоугольник, вычисление площади многоугольник (родительский класс для более конкретных фигур), определение выпуклости многоугольника, поле для длины массива
+{
+    public int Length { get; set; }
+    public Point[] Points { get; set; }
+    public Polygon(Point[]? points)
+    {
+        if (points is not null)
         {
-            X = x ?? 0;
-            Y = y ?? 0;
+            Length = points.Length;
+            Points = points;
+        }
+        else
+        {
+            throw new NullReferenceException("Не задан массив координат вершин многоугольника.");
         }
     }
-    public abstract class Polygon //класс определения понятия многоугольник, вычисление площади многоугольник (родительский класс для более конкретных фигур), определение выпуклости многоугольника, поле для длины массива
+    public bool IsConvex()//проверка многоугольника на выпуклость
     {
-        public int Length { get; set; }
-        public Point[] Points { get; set; }
-        public Polygon(Point[]? points)
+        if (Length < 4)
         {
-            if (points is not null)
-            {
-                Length = points.Length;
-                Points = new Point[points.Length];
-            }
-            else
-            {
-                throw new NullReferenceException("Не задан массив координат вершин многоугольника.");
-            }
-        }
-        public bool IsConvex()//проверка многоугольника на выпуклость
-        {
-            if (Length < 4)
-            {
-                return true;
-            }
-            bool isConvex = false;
-            for (int i = 0; i < Length; i++)
-            {
-                double dx1 = Points[(i + 2) % Length].X - Points[(i + 1) % Length].X;
-                double dy1 = Points[(i + 2) % Length].Y - Points[(i + 1) % Length].Y;
-                double dx2 = Points[i].X - Points[(i + 1) % Length].X;
-                double dy2 = Points[i].Y - Points[(i + 1) % Length].Y;
-                double zcrossproduct = dx1 * dy2 - dy1 * dx2;
-                if (i == 0)
-                {
-                    isConvex = zcrossproduct > 0;
-                }
-                else if (isConvex != (zcrossproduct > 0))
-                {
-                    return false;
-                }
-            }
             return true;
         }
-        public double PolygonSquare()//вычисление площади многугольника по формуле шнуровка гаусса
+        bool isConvex = false;
+        for (int i = 0; i < Length; i++)
         {
-            double polygonSquare = 0;
-            for (int i = 0; i < Length; i++)
+            double dx1 = Points[(i + 2) % Length].X - Points[(i + 1) % Length].X;
+            double dy1 = Points[(i + 2) % Length].Y - Points[(i + 1) % Length].Y;
+            double dx2 = Points[i].X - Points[(i + 1) % Length].X;
+            double dy2 = Points[i].Y - Points[(i + 1) % Length].Y;
+            double zcrossproduct = dx1 * dy2 - dy1 * dx2;
+            if (i == 0)
             {
-                polygonSquare += Points[i].X * Points[i + 1].Y;
+                isConvex = zcrossproduct > 0;
             }
-            for (int i = Length - 1; i < 0; i++)
+            else if (isConvex != (zcrossproduct > 0))
             {
-                polygonSquare -= Points[i].X * Points[i - 1].Y;
+                return false;
             }
-            return polygonSquare;
+        }
+        return true;
+    }
+    public virtual double PolygonSquare()//вычисление площади многугольника по формуле шнуровка гаусса
+    {
+        double polygonSquare = 0;
+        for (int i = 0; i < Length; i++)
+        {
+            polygonSquare += Points[i].X * Points[i + 1].Y;
+        }
+        for (int i = Length - 1; i < 0; i++)
+        {
+            polygonSquare -= Points[i].X * Points[i - 1].Y;
+        }
+        return polygonSquare;
+    }
+}
+public class Circle : Polygon//класс для вычисления площади круга (pi*r^2), массив вершин имеет длину в 2 элемента (центр окружности и точка на окружности), поле для константы пи, перегрузка метода вычисления площади 
+{
+    public const double PI = Math.PI;
+    public Circle(Point[]? points)
+    {
+        if ((points is not null)&&(points.Length == 2))
+        {
+            Points.Length = 2;
+            Points = points;
+        }
+        else
+        {
+            throw new NullReferenceException("Не задан/ы центр окружности и/или точка на окружности .");
         }
     }
-    public class Circle : Polygon//класс для вычисления площади круга (2*pi*r), массив вершин имеет длину в 2 элемента (центр окружности и точка на окружности), поле для константы пи, перегрузка метода вычисления площади 
+    public override double PolygonSquare()// вычисление площади по формуле pi*r^2
     {
-        
+        double polygonSquare = 2 * PI * Math.Pow(Math.Sqrt(Math.Pow(Points[1].X - Points[0].X, 2) + Math.Pow(Points[1].Y - Points[0].Y, 2)), 2);
+        return polygonSquare;
     }
-    public class Triangle : Polygon//класс для вычисления площади (ф-ла из класса Polygon),массив вершин имеет длину в 3 элемента, проверка треугольника на прямоугольность
-    {
+}
+public class Triangle : Polygon//класс для вычисления площади (ф-ла из класса Polygon),массив вершин имеет длину в 3 элемента, проверка треугольника на прямоугольность
+{
 
-    }
 }
